21c7addc6896608dd14e5581a007496852c6fb10
yying8 <yueyan.ying0256@gmail.com>
2024-12-27 12:52:11 -0500

    Update AboutMe.tsx

--- app/AboutMe.tsx
+++ app/AboutMe.tsx
@@ -32,16 +32,21 @@ export default function AboutMe(){
   return(
     <div className="aboutme">
       <Aboutme>
+        <h2>About Me</h2>
         <p>
-          <h2>About Me</h2>
-          Hello👋! My name is Yueyan Ying, and I am a U3 Computer Science student at McGill University. 
+          Hello👋! My name is Yueyan Ying, and I am a U3 Computer Science student at McGill University.
           I am passionate about front-end and back-end development as well as databases. I am always eager to learn new technologies and enhance my skills.
+        </p>
+          
 
-          <h4>Academic Interests</h4>
+        <h4>Academic Interests</h4>
+        <p>
           I am particularly interested in web development, data science, software development, and artificial intelligence. 
           My academic journey is fueled by curiosity and hands-on experiences. I am always looking for opportunities to grow and tackle new challenges.
+        </p>
 
-          <h4>Personal Passions</h4>
+        <h4>Personal Passions</h4>
+        <p>
           Outside of work, I love traveling, playing video games, and spending time with family and friends. 
           Additionally, I am a big movie fan — to date, I have watched over 600 movies! 🎬
         </p>

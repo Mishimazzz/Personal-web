de2b7672dbc32e0756e8d6881d86b5b502c3180c
yying8 <yueyan.ying0256@gmail.com>
2024-12-31 12:25:59 -0500

    update projects page

--- app/Projects.tsx
+++ app/Projects.tsx
@@ -7,20 +7,28 @@ const ProjectsElements = [
         description: "also used React, Node.js, MongoDB"
     },
     {
-        title: "C",
+        title: "C & C++",
         description: "Projects include linux shell, malloc, and more"
     },
     {
-        title: "C",
-        description: "Projects include linux shell, malloc, and more"
+        title: "R",
+        description: "Projects include graph analysis, data visualization, and more"
     },
     {
-        title: "C",
-        description: "Projects include linux shell, malloc, and more"
+        title: "Java",
+        description: "Projects are all from university courses"
     },
     {
-        title: "C",
-        description: "Projects include linux shell, malloc, and more"
+        title: "Python",
+        description: "Projects are all from university courses"
+    },
+    {
+        title: "SQL",
+        description: "Projects include simple database design, query, and more"
+    },
+    {
+        title: "MISP & OCAML",
+        description: "Projects are all from university courses"
     }
 ];
 
@@ -41,7 +49,7 @@ export default function Projects(){
         <div className="project-page">
             <h2>Noted:</h2>
             <p>
-                <span className="tab"></span>Most of my projects are hosted on my GitHub, and they are university projects. According to university policy, I can't share the code here.<br></br>
+                <span className="tab"></span>Most of my projects are hosted on my <a href="https://github.com/Mishimazzz"><strong>GitHub</strong></a>, and they are university projects. According to university policy, <strong>I can't share the code here</strong>.<br></br>
                 <span className="tab"></span>The list below includes work I have done independently. If you are interested, please check it out!<br></br>
                 <span className="tab"></span>If you are interested in what I have done but is private, please contact me with email!
             </p>

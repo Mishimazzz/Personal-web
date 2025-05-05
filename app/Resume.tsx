83671ddd90516689d54bf8a4342bf4ace6365c18
yying8 <yueyan.ying0256@gmail.com>
2025-05-05 14:21:25 -0400

    update Resume.css and tsx

--- app/Resume.tsx
+++ app/Resume.tsx
@@ -2,28 +2,21 @@
 import React, { useState } from "react";
 import './Resume.css';
 
-const Resume1: React.FC = () => {
+const Resume: React.FC = () => {
     const [isZoomed, setIsZoomed] = useState(false);
 
     const handleClick = () => setIsZoomed(!isZoomed);
 
     return (
-        <div className="resume-container">
+        <div className={`resume-container ${isZoomed ? "zoomed" : ""}`}>
             <div
                 className={`resume ${isZoomed ? "zoomed" : ""}`}
                 onClick={handleClick}
             >
                 <img src="/img/yueyan-resume.jpg" alt="yueyan ying resume" />
             </div>
-            {/* <p>(Click the image to {isZoomed ? "zoom out" : "zoom in"})</p> */}
         </div>
     );
 };
 
-export default function Resume() {
-    return (
-        <div>
-            <Resume1 />
-        </div>
-    );
-}
+export default Resume;

6b79cace24dc9fd8900d936b80ebe3a01fc8720b
yying8 <yueyan.ying0256@gmail.com>
2025-08-12 22:11:27 -0400

    save

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 //local host will be commented out
 
-/** @type {import('next').NextConfig} */
 module.exports = {
-    // output: 'export',
-    // images: { unoptimized: true },
+    // assetPrefix: "/Personal-web/",
+    // basePath: "/Personal-web",
 };

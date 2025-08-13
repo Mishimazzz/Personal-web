0b8ab61fe5e4dce3daabdb2a9b79f47c81f84cfd
yying8 <yueyan.ying0256@gmail.com>
2025-08-12 22:15:09 -0400

    save

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 //local host will be commented out
 
+/** @type {import('next').NextConfig} */
 module.exports = {
-    // assetPrefix: "/Personal-web/",
-    // basePath: "/Personal-web",
+    output: 'export',
+    images: { unoptimized: true }, // 用到 next/image 必加
+    // 确保没有 basePath/assetPrefix 自定义前缀
 };

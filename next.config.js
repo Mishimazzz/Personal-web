ba6f62bd193f3a11a48719ebddbf63a0fb6cb947
yying8 <yueyan.ying0256@gmail.com>
2025-08-12 22:17:56 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 //local host will be commented out
 
-/** @type {import('next').NextConfig} */
 module.exports = {
-    output: 'export',
-    images: { unoptimized: true }, // 用到 next/image 必加
-    // 确保没有 basePath/assetPrefix 自定义前缀
+    // assetPrefix: "/Personal-web/",
+    // basePath: "/Personal-web",
 };
